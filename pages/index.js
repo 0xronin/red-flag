aadad99a0a6d0cf559b2512864bd1d05dab5dd3f
Ronin <31029205+0xronin@users.noreply.github.com>
2022-08-15 15:21:12 +0530

    red-flag

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -22,7 +22,7 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-        <a href="https://dk3.in/" className={styles.card} target="_blank">
+          <a href="https://dk3.in/" className={styles.card} target="_blank">
             <h2>dk3 &rarr;</h2>
             <p>
               This Fake Shopping website has fake listing and scams users via
@@ -45,13 +45,17 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a>
-          Setup by 0xRonin | Powered by{' '}
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Setup by 0xRonin | Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
