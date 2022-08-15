8569d2ca30071820c8e4c2aa06e87826beee257f
Ronin <31029205+0xronin@users.noreply.github.com>
2022-08-15 15:26:02 +0530

    red-flag

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://twitter.com/0xZKP"
           target="_blank"
           rel="noopener noreferrer"
         >
